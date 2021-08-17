ce8f6fb56b0949da8d750915b398a67c42ffb1b6
astra <punkx.0gravity@gmail.com>
2021-08-17 22:53:08 +0545

    Update CIS-nginx.sh

--- CIS-nginx.sh
+++ CIS-nginx.sh
@@ -22,116 +22,116 @@
 ## This section defines global variables used in the script
 printf "Enter name of File:"
 read -r name
-echo "Initial Setup" >> $name 
+echo "1. Initial Setup" >> $name 
 nginx -v >> $name
-echo "Configure Software Updates" >> $name
+echo "2. Configure Software Updates" >> $name
 yum repolist -v nginx >> $name
-echo "Ensure the latest software package is installed (Not Scored)" >> $name
+echo "3. Ensure the latest software package is installed (Not Scored)" >> $name
 yum info nginx >> $name
-echo "Ensure only required modules are installed" >> $name
+echo "4. Ensure only required modules are installed" >> $name
 nginx -V >> $name
-echo "Ensure HTTP WebDAV module is not installed (Scored)" >> $name
+echo "5. Ensure HTTP WebDAV module is not installed (Scored)" >> $name
 nginx -V 2>&1 | grep http_dav_module >> $name
-echo "Ensure modules with gzip functionality are disabled (Scored)" >> $name
+echo "6. Ensure modules with gzip functionality are disabled (Scored)" >> $name
 nginx -V | grep 'http_gzip_module\|http_gzip_static_module' >> $name
-echo "Ensure the autoindex module is disabled (Scored)" >> $name
+echo "7. Ensure the autoindex module is disabled (Scored)" >> $name
 egrep -i '^\s*autoindex\s+' /etc/nginx/nginx.conf >> $name
 egrep -i '^\s*autoindex\s+' /etc/nginx/conf.d/* >> $name
-echo "Ensure that NGINX is run using a non-privileged, dedicated service account (Not Scored)" >> $name
+echo "8. Ensure that NGINX is run using a non-privileged, dedicated service account (Not Scored)" >> $name
 grep "user[^;]*;" /etc/nginx/nginx.conf >> $name
 grep "user[^;]*;" /etc/nginx/nginx.conf >> $name
 sudo -l -U nginx >> $name
-echo "Ensure the NGINX service account is locked (Scored)" >> $name
+echo "9. Ensure the NGINX service account is locked (Scored)" >> $name
 passwd -S nginx >> $name
-echo "Ensure the NGINX service account has an invalid shell (Scored)" >> $name
+echo " 10. Ensure the NGINX service account has an invalid shell (Scored)" >> $name
 grep nginx /etc/passwd >> $name
-echo "Ensure NGINX directories and files are owned by root (Scored)" >> $name
+echo "11. Ensure NGINX directories and files are owned by root (Scored)" >> $name
 stat /etc/nginx >> $name
-echo "Ensure access to NGINX directories and files is restricted (Scored)" >> $name
+echo "12. Ensure access to NGINX directories and files is restricted (Scored)" >> $name
 find /etc/nginx -type d | xargs ls -ld >> $name
-echo "Ensure the NGINX process ID (PID) file is secured (Scored)" >> $name
+echo "13. Ensure the NGINX process ID (PID) file is secured (Scored)" >> $name
 ls -l /var/run/nginx.pid >> $name
-echo "Ensure the core dump directory is secured (Not Scored)" >> $name
+echo "14. Ensure the core dump directory is secured (Not Scored)" >> $name
 grep working_directory /etc/nginx/nginx.conf >> $name
-echo "Ensure NGINX only listens for network connections on authorized ports (Not Scored)" $name
+echo "15. Ensure NGINX only listens for network connections on authorized ports (Not Scored)" $name
 grep -ir listen /etc/nginx >> $name
-echo "Ensure requests for unknown host names are rejected (Not Scored)" >> $name
+echo "16. Ensure requests for unknown host names are rejected (Not Scored)" >> $name
 curl -k -v https://127.0.0.1 -H 'Host: invalid.host.com' >> $name
-echo "Ensure keepalive_timeout is 10 seconds or less, but not 0 (Scored)" >> $name
+echo "17. Ensure keepalive_timeout is 10 seconds or less, but not 0 (Scored)" >> $name
 grep -ir keepalive_timeout /etc/nginx >> $name
-echo "Ensure send_timeout is set to 10 seconds or less, but not 0 (Scored)" >> $name
+echo "18. Ensure send_timeout is set to 10 seconds or less, but not 0 (Scored)" >> $name
 grep -ir send_timeout /etc/nginx >> $name
-echo "Information Disclosure" >> $name
-echo "Ensure server_tokens directive is set to `off` (Scored)" >> $name
+echo "19. Information Disclosure" >> $name
+echo "20. Ensure server_tokens directive is set to `off` (Scored)" >> $name
 curl -I 127.0.0.1 | grep -i server >> $name
-echo "Ensure default error and index.html pages do not reference NGINX (Scored)" >> $name
+echo "21. Ensure default error and index.html pages do not reference NGINX (Scored)" >> $name
 grep -i nginx /usr/share/nginx/html/index.html >> $name
 grep -i nginx /usr/share/nginx/html/50x.html >> $name
-echo "Ensure hidden file serving is disabled (Not Scored)" >> $name
+echo "22. Ensure hidden file serving is disabled (Not Scored)" >> $name
 grep location /etc/nginx/nginx.conf >> $name
-echo "Ensure the NGINX reverse proxy does not enable information disclosure (Scored)" >> $name
+echo "23. Ensure the NGINX reverse proxy does not enable information disclosure (Scored)" >> $name
 grep proxy_hide_header /etc/nginx/nginx.conf >> $name
-echo "Logging" >> $name 
-echo "Ensure access logging is enabled (Scored)" >> $name
+echo "24. Logging" >> $name 
+echo "25. Ensure access logging is enabled (Scored)" >> $name
 grep -ir access_log /etc/nginx >> $name
-echo "Ensure error logging is enabled and set to the info logging level (Scored)" >> $name
+echo "26. Ensure error logging is enabled and set to the info logging level (Scored)" >> $name
 grep error_log /etc/nginx/nginx.conf >> $name
-echo "Ensure log files are rotated (Scored)" >> $name
+echo "27. Ensure log files are rotated (Scored)" >> $name
 cat /etc/logrotate.d/nginx | grep weekly >> $name
 cat /etc/logrotate.d/nginx | grep rotate >> $name
-echo "Ensure error logs are sent to a remote syslog server (Not Scored)" >> $name
+echo "28. Ensure error logs are sent to a remote syslog server (Not Scored)" >> $name
 grep -ir syslog /etc/nginx >> $name
-echo "Ensure proxies pass source IP information (Scored)" >> $name
+echo "29. Ensure proxies pass source IP information (Scored)" >> $name
 proxy_set_header X-Real-IP $remote_addr; >> $name
 proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for; >> $name 
-echo "Ensure a trusted certificate and trust chain is installed (Not Scored)" >> $name
+echo "30. Ensure a trusted certificate and trust chain is installed (Not Scored)" >> $name
 grep -ir ssl_certificate /etc/nginx/ >> $name
-echo "Ensure only modern TLS protocols are used (Scored)" >> $name
+echo "31. Ensure only modern TLS protocols are used (Scored)" >> $name
 grep -ir ssl_protocol /etc/nginx >> $name
-echo "Disable weak ciphers (Scored)" >> $name
+echo "32. Disable weak ciphers (Scored)" >> $name
 grep -ir ssl_ciphers /etc/nginx/ && grep -ir proxy_ssl_ciphers /etc/nginx >> $name
-echo "Ensure custom Diffie-Hellman parameters are used (Scored)" >> $name
+echo "33. Ensure custom Diffie-Hellman parameters are used (Scored)" >> $name
 grep ssl_dhparam /etc/nginx/nginx.conf >> $name
-echo "Ensure Online Certificate Status Protocol (OCSP) stapling is enabled (Scored)" >> $name
+echo "34. Ensure Online Certificate Status Protocol (OCSP) stapling is enabled (Scored)" >> $name
 grep -ir ssl_stapling /etc/nginx >> $name
-echo "Ensure HTTP Strict Transport Security (HSTS) is enabled (Scored)" >> $name
+echo "35. Ensure HTTP Strict Transport Security (HSTS) is enabled (Scored)" >> $name
 grep -ir Strict-Transport-Security /etc/nginx >> $name
-echo "Ensure HTTP Public Key Pinning is enabled (Not Scored)" >> $name
+echo "36. Ensure HTTP Public Key Pinning is enabled (Not Scored)" >> $name
 grep -ir Public-Key-Pins /etc/nginx >> $name
-echo "Ensure upstream server traffic is authenticated with a client certificate (Scored)" >> $name
+echo "37. Ensure upstream server traffic is authenticated with a client certificate (Scored)" >> $name
 grep -ir proxy_ssl_certificate /etc/nginx >> $name
-echo "Ensure the upstream traffic server certificate is trusted (Not Scored)" >> $name
+echo "38. Ensure the upstream traffic server certificate is trusted (Not Scored)" >> $name
 grep -ir proxy_ssl_trusted_certificate /etc/nginx >> $name
 grep -ir proxy_ssl_verify /etc/nginx >> $name
-echo "Ensure HTTP Public Key Pinning is enabled (Not Scored)" >> $name
+echo "39. Ensure HTTP Public Key Pinning is enabled (Not Scored)" >> $name
 grep -ir Public-Key-Pins /etc/nginx >> $name
-echo "Ensure upstream server traffic is authenticated with a client certificate (Scored)" >> $name
+echo "40. Ensure upstream server traffic is authenticated with a client certificate (Scored)" >> $name
 grep -ir proxy_ssl_certificate /etc/nginx >> $name
-echo "Ensure the upstream traffic server certificate is trusted (Not Scored)" >> $name
+echo "41. Ensure the upstream traffic server certificate is trusted (Not Scored)" >> $name
 grep -ir proxy_ssl_trusted_certificate /etc/nginx >> $name
 grep -ir proxy_ssl_verify /etc/nginx >> $name
-echo "Ensure session resumption is disabled to enable perfect forward security (Scored)" >> $name
+echo "42. Ensure session resumption is disabled to enable perfect forward security (Scored)" >> $name
 grep -ir ssl_session_tickets /etc/nginx >> $name
-echo "Ensure HTTP/2.0 is used (Not Scored)"
+echo "43. Ensure HTTP/2.0 is used (Not Scored)"
 grep -ir http2 /etc/nginx >> $name
-echo "Ensure only whitelisted HTTP methods are allowed (Not Scored)"
+echo "44. Ensure only whitelisted HTTP methods are allowed (Not Scored)"
 curl -X DELETE http://localhost/index.html >> $name
 curl -X GET http://localhost/index.html >> $name
-echo "Request Limits" >> $name
-echo "Ensure timeout values for reading the client header and body are set correctly (Scored)" >> $name
+echo "45. Request Limits" >> $name
+echo "46. Ensure timeout values for reading the client header and body are set correctly (Scored)" >> $name
 grep -ir timeout /etc/nginx >> $name
-echo "Ensure the maximum request body size is set correctly (Scored)" >> $name
+echo "47. Ensure the maximum request body size is set correctly (Scored)" >> $name
 grep -ir client_max_body_size /etc/nginx >> $name
-echo "Ensure the maximum buffer size for URIs is defined (Scored)" >> $name
+echo "48. Ensure the maximum buffer size for URIs is defined (Scored)" >> $name
 grep -ir large_client_header_buffers /etc/nginx/ >> $name
-echo "Browser Security" >> $name
-echo "Ensure X-Frame-Options header is configured and enabled (Scored)" >> $name
+echo "49. Browser Security" >> $name
+echo "50. Ensure X-Frame-Options header is configured and enabled (Scored)" >> $name
 grep -ir X-Frame-Options /etc/nginx >> $name
-echo "Ensure X-Content-Type-Options header is configured and enabled (Scored)" >> $name
+echo "51. Ensure X-Content-Type-Options header is configured and enabled (Scored)" >> $name
 grep -ir X-Content-Type-Options /etc/nginx >> $name
-echo "Ensure the X-XSS-Protection Header is enabled and configured properly (Scored)" >> $name
+echo "52. Ensure the X-XSS-Protection Header is enabled and configured properly (Scored)" >> $name
 grep -ir X-Xss-Protection /etc/nginx >> $name
-echo "Ensure that Content Security Policy (CSP) is enabled and configured properly (Not Scored)" >> $name
+echo "53. Ensure that Content Security Policy (CSP) is enabled and configured properly (Not Scored)" >> $name
 grep -ir Content-Security-Policy /etc/nginx >> $name
-echo "Ensure the Referrer Policy is enabled and configured properly (Not Scored)" >> $name
+echo "54. Ensure the Referrer Policy is enabled and configured properly (Not Scored)" >> $name
 grep -r Referrer-Policy /etc/nginx >> $name
